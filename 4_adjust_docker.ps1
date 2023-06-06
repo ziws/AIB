3dce70040d24192d260e01e42fd8461cd7c000e9
ziw <43388852+ziws@users.noreply.github.com>
2023-06-06 22:07:08 +0200

    Update 4_adjust_docker.ps1

--- 4_adjust_docker.ps1
+++ 4_adjust_docker.ps1
@@ -6,6 +6,11 @@ Write-Host "Switched to Windows containers" -ForegroundColor Green
 Sleep 10
 
 # Docker Desktop - Engine adjustment
+# Write-Host "Adjust Docker Engine DNS config..."
+# echo '{"dns":["8.8.8.8"],"experimental":false}' > $env:USERPROFILE\.docker\windows-daemon.json
+# Write-Host "Docker Engine DNS config adjustted" -ForegroundColor Green
+
 Write-Host "Adjust Docker Engine DNS config..."
-echo '{"dns":["8.8.8.8"],"experimental":false}' > $env:USERPROFILE\.docker\windows-daemon.json
+invoke-webrequest -uri 'https://raw.githubusercontent.com/ziws/AIB/main/windows-daemon.json' -OutFile 'c:\\ImageBuilder\\windows-daemon.json',
+copy-item 'c:\\ImageBuilder\\windows-daemon.json\\' -Destination '$env:USERPROFILE\\.docker\\'
 Write-Host "Docker Engine DNS config adjustted" -ForegroundColor Green
