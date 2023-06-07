3955f28e7263298f4ca81ec9c67e324b33920619
ziw <43388852+ziws@users.noreply.github.com>
2023-06-07 10:16:55 +0200

    Update 6_Windows10_boot_fix.ps1

--- 6_Windows10_boot_fix.ps1
+++ 6_Windows10_boot_fix.ps1
@@ -1,1 +1,19 @@
+#region Set logging 
+$logFile = "c:\ImageBuilder\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
+function Write-Log {
+    Param($message)
+    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
+}
+#endregion
 
+#region Sysprep Fix
+# Fix for first login delays due to Windows Module Installer
+try {
+    ((Get-Content -path C:\DeprovisioningScript.ps1 -Raw) -replace 'Sysprep.exe /oobe /generalize /quiet /quit', 'Sysprep.exe /oobe /generalize /quit /mode:vm' ) | Set-Content -Path C:\DeprovisioningScript.ps1
+    write-log "Sysprep Mode:VM fix applied"
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error updating script: $ErrorMessage"
+}
+#endregion
